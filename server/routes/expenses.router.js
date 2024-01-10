b327501caae7886d433875a5b4febb6dbe4343ee
AndyPrehn <133592836+AndyPrehn@users.noreply.github.com>
2024-01-10 11:55:01 -0600

    expenses post into database is working

--- server/routes/expenses.router.js
+++ server/routes/expenses.router.js
@@ -15,7 +15,27 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   // POST route code here
   console.log('in expenses post', req.body)
-  res.sendStatus (201)
+  let queryText = `INSERT INTO "Expenses" ("account", "date", "status", "payee", "category", "amount")
+  VALUES ($1, $2, $3, $4, $5, $6);`;
+ 
+  pool.query(queryText, [
+      req.body.account,
+      req.body.date,
+      req.body.status,
+      req.body.payee,
+      req.body.category,
+    req.body.amount])
+      .then(result => {
+          res.sendStatus(200);
+          console.log('POST successful.');
+      })
+      .catch(error => {
+          console.error(error);
+          res.sendStatus(500);
+
+      });
 });
 
+
 module.exports = router;
+
