05aa6e51b38079b7ea63de60fcf12381edb9a438
AndyPrehn <133592836+AndyPrehn@users.noreply.github.com>
2024-01-14 10:30:25 -0600

    Expenses get route is displaying on the DOM YAY

--- src/components/ExpensesList/ExpensesList.jsx
+++ src/components/ExpensesList/ExpensesList.jsx
@@ -2,43 +2,92 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import PropTypes from 'prop-types';
+import { alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import { visuallyHidden } from '@mui/utils';
+
 function ExpensesList() {
+    const [expensesList, setExpensesList] = useState([]);
 
-    // const dispatch = useDispatch();
-    // const StatsList = useSelector(store => store.statsList);
-
-    // const [sortOrder, setSortOrder] = useState('asc');
-    // const [sortedColumn, setSortedColumn] = useState('id');
-
-    const columns = [
-        { id: 'account', label: 'Account' },
-        { id: 'date', label: 'Date' },
-        { id: 'status', label: 'Status' },
-        { id: 'payee', label: 'Payee' },
-        { id: 'category', label: 'Category' },
-        { id: 'amount', label: 'Amount' },
-    ];
+    // const columns = [
+    //     { id: 'account', label: 'Account' },
+    //     { id: 'date', label: 'Date' },
+    //     { id: 'status', label: 'Status' },
+    //     { id: 'payee', label: 'Payee' },
+    //     { id: 'category', label: 'Category' },
+    //     { id: 'amount', label: 'Amount' },
+    // ];
 
     useEffect(() => {
-        dispatch({ type: 'FETCH_STATS' })
+        //make initial fetch of expenses
+        getExpensesList();
     }, []);
 
+    function getExpensesList(){
+            axios.get('/api/expenses').then((response) => {
+                console.log('Expenses are broken', response);
+                console.log(response.data);
+                setExpensesList(response.data);
+            })
+            .catch((error) => {
+                console.log("Error in GET '/expenses inside getExpensesList", error);
+                alert("Hi, its me, Im the problem");
+            })
+        }
 
 return (
-    <form onSubmit={handleSubmit}>
-    Account: <input value={account} placeholder='Account' onChange={(e)=>setAccount(e.target.value)}/>
-    <br></br>
-    Date: <input value={date} type="date" placeholder='Date of Transaction' onChange={(e)=>setDate(e.target.value)}/>
-    <br></br>
-    Status: <input value={status} placeholder='Status' onChange={(e)=>setStatus(e.target.value)}/>
-    <br></br>
-    Payee: <input value={payee} placeholder='Payee' onChange={(e)=>setPayee(e.target.value)}/>
-    <br></br>
-    Category: <input value={category} placeholder='Category' onChange={(e)=>setCategory(e.target.value)}/>
-    <br></br>
-    Amount: <input value={amount} placeholder='Amount' onChange={(e)=>setAmount(e.target.value)}/>
-    <br></br>
-
-  </form>
-  );
+    // map over the esxpenseslist array
+        // render on the DOM using tags
+        <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell alighn="center"> Account </TableCell>
+              <TableCell align="center">Date</TableCell>
+              <TableCell align="center">Status</TableCell>
+              <TableCell align="center">Payee</TableCell>
+              <TableCell align="center">Category</TableCell>
+              <TableCell align="center">Amount</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {expensesList.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.account}
+                </TableCell>
+                <TableCell align="center">{row.date}</TableCell>
+                <TableCell align="center">{row.status}</TableCell>
+                <TableCell align="center">{row.payee}</TableCell>
+                <TableCell align="center">{row.category}</TableCell>
+                <TableCell align="center">{row.amount}</TableCell>
+            
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
 }export default ExpensesList;
--- src/components/ExpensesPage/ExpensesPage.jsx
+++ src/components/ExpensesPage/ExpensesPage.jsx
@@ -1,21 +1,13 @@
-import ExpensesForm from '../ExpensesForm/ExpensesForm';
-import ExpensesList from '../ExpensesList/ExpensesList'
+import ExpensesForm from "../ExpensesForm/ExpensesForm";
+import ExpensesList from "../ExpensesList/ExpensesList";
 
-function ExpensesPage(){
-    return(
-        <ExpensesForm /> 
-    )
+function ExpensesPage() {
+  return (
+    <div>
+      <ExpensesForm />
+      <ExpensesList />
+    </div>
+  );
 }
 
-// function getExpensesList(){
-//     axios.get('/expensesList').then((response) => {
-//         console.log('Expenses are broken', response);
-//         console.log(response.data);
-//         setExpensesList(response.data);
-//     })
-//     .catch((error) => {
-//         console.log("Error in GET '/expenses inside getExpensesList", error);
-//         alert("Hi, its me, Im the problem");
-//     })
-// }
 export default ExpensesPage;
