44979fc1a2e24074b235c26e9d4772e1631c9dd3
Andy Prehn <133592836+AndyPrehn@users.noreply.github.com>
2025-05-02 08:26:50 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.use(express.static('build'));
 // App Set //
 const PORT = process.env.PORT || 5072;
 
-/** Listen * */
+/** Listen */
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
